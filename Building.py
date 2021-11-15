f8ba59584eae00ed7e3e04114d1680a6d6d0732e
gars-cmd <avidouille@gmail.com>
2021-11-15 19:25:42 +0200

    made modification to Building Class

--- Building.py
+++ Building.py
@@ -20,9 +20,21 @@ class Building:
 
     def getElevator(self , i)->Elevator :
         return self.elevators[i]
-
     
-
+    def elevatorsCategories(self):
+        listOfSpeeder= []
+        listOfSlower =[]
+        sum = 0
+        average = 0
+        for elevator in range(self.numOfElevators()):
+            sum+= Elevator.Elevator.getSpeed(self.getElevator(elevator))
+        average = sum/self.numOfElevators()
+        for elevator in range(self.numOfElevators()):
+            if Elevator.Elevator.getSpeed(self.getElevator(elevator))>average:
+                listOfSpeeder.append(elevator)
+            else:
+                listOfSlower.append(elevator)        
+        return [listOfSpeeder , listOfSlower]
 
 
     
