dcf56e2290b3fd11a7f4df533eb0411d1ed57e68
gars-cmd <avidouille@gmail.com>
2021-11-17 12:25:19 +0200

    add final version of Ex1

--- Algo.py
+++ Algo.py
@@ -35,33 +35,27 @@ def csvToCall(file):
         print("error in reading file")
     return listCalls        
 
-def allocateAnElevator(b:Building , c:Call):    #algorithm that allocated according to speed of the elevators
+# def allocateAnElevator(b:Building , c:Call):    #algorithm that allocated according to speed of the elevators
     
-    choosen=0
-    if Building.numOfElevators(b)==1:
-        return choosen
-    else:
-        Categories = Building.elevatorsCategories(b)
+#     choosen=0
+#     if Building.numOfElevators(b)==1:
+#         return choosen
+#     else:
+#         Categories = Building.elevatorsCategories(b)
         
-        listOfSpeeder = Categories[0]
-        # print(len(listOfSpeeder))
-        listOfSlower = Categories[1]
-        # print(len(listOfSlower))
-        q = abs((b.minFloor-b.maxFloor)+1)/b.numOfElevators()
-        # print(q)
-        if abs(int(Call.getSrc(c))-int(Call.getDest(c))) > q*2:
-            # print("la")
-            global count0,count1
-            choosen = count0 % len(listOfSpeeder)+1
-            count0=count0+1
-            # print(count0)
-        else:
-            # print("la bas ")
-            choosen = count1 % len(listOfSlower)+1
-            count1 = count1 + 1
-            # print(count1)
-            
-    return choosen
+#         listOfSpeeder = Categories[0]
+#         listOfSlower = Categories[1]
+
+#         q = abs((b.minFloor-b.maxFloor)+1)/b.numOfElevators()
+#         if abs(int(Call.getSrc(c))-int(Call.getDest(c))) > q*2:
+#             global count0,count1
+#             choosen = count0 % (len(listOfSpeeder)+1)
+#             count0=count0+1
+#         else:
+#             choosen = count1 % (len(listOfSlower)+1)
+#             count1 = count1 + 1
+#     print(choosen)        
+#     return choosen
 
 def allocateAnElevator2(b:Building , c:Call):    
     choosen = 0
@@ -74,21 +68,17 @@ def allocateAnElevator2(b:Building , c:Call):
         for elevator in range(rangeElev):
             if elevator<(rangeElev/2):
                 elevatorUp.append(elevator)
-                print(elevator)
                 
             else:
                 elevatorDown.append(elevator)
-                # print(elevator)
         global count0,count1
-        # print(len(elevatorUp),len(elevatorDown))
-        # print(Call.getType(c))
         if Call.getType(c)==1:
             choosen = elevatorUp[count0 % (len(elevatorUp))]
             count0=count0+1
         else:
             choosen= elevatorDown[count1 % (len(elevatorDown))]
             count1 = count1+1
-    # print(choosen)
+    print(choosen)
     return choosen
             
     
@@ -96,32 +86,23 @@ def writeOnCsv(fileToWriteOn , listcall:list):
     with open (fileToWriteOn , 'w' , newline= '') as f:
         writer = csv.writer(f)
         for calls in listcall:
-            writer.writerow(['Elevator call' , Call.getTime(calls) , Call.getSrc(calls) , Call.getDest(calls) , Call.allocatedTo(calls)])
+            writer.writerow(['Elevator call' , Call.getTime(calls) , Call.getSrc(calls) , Call.getDest(calls) ,-1 ,  Call.allocatedTo(calls)])
               
     
 
 if __name__ == "__main__":
-    # print("coucou")
     fileBuild = sys.argv[1]
     fileCall = sys.argv[2]
     fileOut = sys.argv[3]
 
     b = jsonToBuilding(fileBuild)
     listCall = csvToCall(fileCall)   
-    # listUpCall = []
-    # listDownCall = []
-    # for item in listCall:
-    #     if (Call.getType(item) == 1):
-    #         listUpCall.append(item)
-    #     else:
-    #         listDownCall.append(item)    
+     
     
     
     for item in listCall:
         Call.setAllocate(item,allocateAnElevator2(b,item))
-        # print(Call.getType(item) , Call.allocatedTo(item) )
-        # print(Call.allocatedTo(item))
-        
+       
     writeOnCsv('test.csv' , listCall)
         
          
